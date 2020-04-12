d6e34740ae3d69f3fa8e17feb83921c4dc370f31
Gabriel Russell <gabrielrussell@protonmail.com>
2020-04-11 20:16:03 -0400

    Start ATA Driver

--- drivers/ata/ata.c
+++ drivers/ata/ata.c
@@ -15,6 +15,28 @@ void ata_device_ready_poll(void) {
   return;
 }
 
-void ata_send_command(uint8_t command) {
-  
+void ata_send_command(uint16_t command) {
+  /*
+    Wait til the device is ready, disable interrupts,
+    then issue the command, restore interrupts
+  */
+  ata_busy_poll();
+  asm volatile("cli");
+  ata_device_ready_poll();
+  outw(COMMAND_REGISTER, command);
+  asm volatile("sti");
+}
+
+void ata_read_sector(uint32_t lba, uint8_t sectors, unsigned char *buf) {
+  outw(DRIVE_HEAD_REGISTER, (uint16_t) (lba >> 24 | 244) );
+  outb(SECTOR_COUNT_REGISTER, sectors);
+  outb(LBA_LO_REGISTER, (uint8_t) lba & 0x000000FF);
+  outb(LBA_MI_REGISTER, (uint8_t) (lba & 0x0000FF00 >> 8));
+  outb(LBA_HI_REGISTER, (uint8_t) (lba & 0x00FF0000 >> 16));
+  ata_send_command(READ_SECTORS_WITH_RETRY);
+
+  while (inb(STATUS_REGISTER) & DATA_TRANSFER_REQUESTED_MASK == 8);
+  for (int i = 0; i < sectors * 512; i++) {
+    buf[i] = inb(DATA_REGISTER);
+  }
 }
